92d9f33f7d4793397ae5929f2b9c0f79b30d4751
Nihal Potdar <Nihalpot2002@gmail.com>
2023-07-15 17:45:10 -0700

    modified sql client to use object store

--- memory/MemoryService.ts
+++ memory/MemoryService.ts
@@ -1,12 +1,14 @@
 import fetch from "node-fetch";
 import { PineconeClient } from "@pinecone-database/pinecone";
 import mysql, { OkPacket, RowDataPacket } from "mysql2/promise";
+import { GetObjectCommand, GetObjectCommandOutput, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { v4 as uuidv4 } from 'uuid';
 
 class MemoryService {
   apiKey: string;
   pinecone: PineconeClient;
   index: string;
-  sqlClient: mysql.Pool;
+  s3Client: S3Client;
 
   constructor() {
     this.apiKey = process.env.OPENAI_API_KEY || "";
@@ -16,13 +18,12 @@ class MemoryService {
       apiKey: process.env.PINECONE_API_KEY || "",
     });
     this.index = process.env.PINECONE_INDEX || "";
-
-    this.sqlClient = mysql.createPool({
-      host: 'localhost',
-      user: process.env.SQL_USER,
-      password: process.env.SQL_PASSWORD,
-      database: 'dungeon_master',
-      port: 3306,
+    this.s3Client = new S3Client({
+      region: "us-east-2",
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+      }
     });
   }
 
@@ -48,21 +49,40 @@ class MemoryService {
     return result.data[0].embedding;
   }
 
+  async storeS3Bucket(content: string, importance: number, timestamp: Date) {
+    const key: string = uuidv4();
+    const response = await this.s3Client.send(new PutObjectCommand({
+      Bucket: process.env.S3_BUCKET_NAME || "",
+      Body: JSON.stringify({
+        content: content,
+        importance: importance,
+        timestamp: timestamp
+      }),
+      Key: key,
+    }));
+    return key;
+  }
+
+  async retrieveS3Bucket(eTag: string) {
+    const response : GetObjectCommandOutput = await this.s3Client.send(new GetObjectCommand({
+      Bucket: process.env.S3_BUCKET_NAME || "",
+      Key: eTag,
+    })); 
+    let body = await response.Body?.transformToString('utf-8') || "";
+    const bodyObj = JSON.parse(body);
+    return (bodyObj.content, bodyObj.importance, bodyObj.timestamp);
+  }
+
   async store(text: string, importance: number, sessionToken?: string) {
     const embedding = await this.getEmbedding(text);
     const timestamp = new Date();
 
-    const [result] = await this.sqlClient.query<OkPacket>(
-      "INSERT INTO memories (content, importance, timestamp) VALUES (?, ?, ?)",
-      [text, importance, timestamp]
-    );
-
-    const id = result.insertId;
+    const id = await this.storeS3Bucket(text, importance, timestamp);
 
     const index = this.pinecone.Index(this.index);
     await index.upsert({
       upsertRequest: {
-        vectors: [{ id: id.toString(), values: embedding }],
+        vectors: [{ id: id, values: embedding }],
         namespace: sessionToken,
       },
     });
@@ -72,12 +92,7 @@ class MemoryService {
     const embedding = await this.getEmbedding(text);
     const timestamp = new Date();
 
-    const [result] = await this.sqlClient.query<OkPacket>(
-      "INSERT INTO memories (content, importance, timestamp) VALUES (?, ?, ?)",
-      [text, 0, timestamp]
-    );
-
-    const id = result.insertId;
+    const id = await this.storeS3Bucket(text, 0, timestamp);
 
     const index = this.pinecone.Index(this.index);
     await index.upsert({
@@ -105,15 +120,16 @@ class MemoryService {
     if (ids.length === 0) {
       return [];
     }
-    const [rows] = await this.sqlClient.query<RowDataPacket[]>(
-      "SELECT id, content, importance FROM memories WHERE id IN (?)",
-      [ids]
-    );
-
-    return rows.map((row) => ({
-      content: row.content,
-      importance: row.importance,
-    }));
+    let res = [];
+    for (let i = 0; i < ids.length; i++) {
+      const result = await this.retrieveS3Bucket(ids[i]);
+      res.push({
+        content: result.content,
+        importance: result.importance,
+      });
+    }
+    console.log(res);
+    return res;
   }
 
 async retrieveStory(query: string, n = 3) {
@@ -130,15 +146,16 @@ async retrieveStory(query: string, n = 3) {
 
 
     const ids = queryResponse?.matches?.map((match) => match.id) || [];
-    const [rows] = await this.sqlClient.query<RowDataPacket[]>(
-      "SELECT id, content, importance FROM memories WHERE id IN (?)",
-      [ids]
-    );
-
-    return rows.map((row) => ({
-      content: row.content,
-      importance: row.importance,
-    }));
+    let res = [];
+    for (let i = 0; i < ids.length; i++) {
+      const result = await this.retrieveS3Bucket(ids[i]);
+      res.push({
+        content: result.content,
+        importance: result.importance,
+      });
+    }
+    console.log(res);
+    return res;
   }
 }
 
