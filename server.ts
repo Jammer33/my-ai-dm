8d1edf3e77149c1fe5b9c7934e7eb306da9aba8a
Vansh Mago <vanshmago9@gmail.com>
2023-07-23 23:16:23 -0700

    Implemented auth functions

--- server.ts
+++ server.ts
@@ -1,27 +1,31 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { config } from 'dotenv';
-(async () => {
-  await config({path: '.env'});
-})();
-import MemoryModule from './services/MemoryService';
+import express from "express";
+import bodyParser from "body-parser";
+import { config } from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
 
-import dungeonMasterRoutes from './routes/dungeonMaster';
+import authRoutes from "./routes/auth";
+
+(async () => {
+  await config({ path: ".env" });
+})();
+import MemoryModule from "./services/MemoryService";
+
+import dungeonMasterRoutes from "./routes/dungeonMaster";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cors());
 
-app.use('/dungeon-master', dungeonMasterRoutes);
+app.use("/dungeon-master", dungeonMasterRoutes);
 
+app.use("/auth", authRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.post("/memories", async (req, res) => {
@@ -36,7 +40,18 @@ app.post("/memories/search", async (req, res) => {
   res.json(memories);
 });
 
-
 function formatInputAndOutput(input: string, output: string) {
   return `User: ${input} DM: ${output}`;
 }
+
+const CONNECTION_URL =
+  "mongodb+srv://vanshmago9:CzUABSQeZzj6G2Ye@cluster0.ktz6ecl.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    })
+  )
+  .catch((error) => console.log(error.message));
